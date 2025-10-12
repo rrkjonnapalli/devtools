679938bfc6246bc3aca22b60785550f4c0e1a8e5
rrkjonnapalli <r.ravikiranjonnapalli@gmail.com>
2025-10-12 18:56:25 +0530

    update vite config for lazy routing

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,13 +6,14 @@ import ToolsPage from '../pages/ToolsPage';
 import Loading from '@shared/Loading/Loading';
 import { tools, type Tool } from './tools';
 import NotFound from '@/pages/NotFound';
+import AboutPage from '@/pages/About';
 
 
 const getRoute = (tool: Tool): RouteObject | null => {
   if (!tool.page) {
     return null;
   }
-  const Page = lazy(() => import(`../pages/${tool.page}`));
+  const Page = lazy(() => import(`../pages/${tool.page}.tsx`));
   return {
     path: tool.id,
     element: (
@@ -27,11 +28,11 @@ export const router = createBrowserRouter([
   {
     // This path uses the layout for all its children
     path: '/',
-    element: <Layout />, // Layout includes header, footer, and breadcrumbs
+    Component: Layout, // Layout includes header, footer, and breadcrumbs
     children: [
       {
         index: true, // Renders at the path "/"
-        element: <ToolsPage />,
+        Component: ToolsPage
       },
       // {
       //   path: 'json-formatter', // Full path: /json-formatter
@@ -55,14 +56,14 @@ export const router = createBrowserRouter([
           {
             index: true,
             path: '', // Full path: /tools
-            element: <ToolsPage />,
+            Component: ToolsPage,
           },
           ...tools.map(tool => getRoute(tool)).filter((route): route is RouteObject => route !== null),
         ]
       },
       {
         path: 'about', // Full path: /about
-        Component: lazy(() => import('../pages/About')), // Lazy load AboutPage
+        Component: AboutPage, // Lazy load AboutPage
       }
       // Add more tool routes here...
     ],
